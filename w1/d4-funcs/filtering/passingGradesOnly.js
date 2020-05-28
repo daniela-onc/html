81814db85c8953a05e5d3b35a58c77aed4277362
Daniela <danielaoancea2009@yahoo.ca>
2020-05-28 19:30:06 +0000

    text

---
+++ w1/d4-funcs/filtering/passingGradesOnly.js
@@ -0,0 +1,11 @@
+const grades = [73, 69, 3, 100, 50, 70, 69, 88, 95, 77, 35];
+//Write code to filter this list down to only passing grades.
+
+//Use the Array filter function to print out only the passing grades
+    //Start by declaring the callback function and then later refactor it to be inline and anonymous, or start with an anonymous function to begin with.
+//Convert the anonymous function into an arrow function in order to get some practice with them.
+
+const passingGradesOnly = grades.filter(function(num) {
+  return (num % 2 == 0);
+});
+console.log('List of passing Grades:', passingGradesOnly);
