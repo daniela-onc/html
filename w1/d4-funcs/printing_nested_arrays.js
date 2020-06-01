2b08726f892208a9e435e6e1967c7042339ab0c7
Daniela <danielaoancea2009@yahoo.ca>
2020-06-01 01:55:11 +0000

    text

--- w1/d4-funcs/printing_nested_arrays.js
+++ w1/d4-funcs/printing_nested_arrays.js
@@ -1,8 +1,48 @@
-const printItems = function(array) {
-  for (let item of array) {
-    console.log(item);
-  }
-}
 
-const array = ["😎", "💩", "🤗", "😼", "😂"];
+//this code print: first array as an simple array and the second array as nested arrays
+//  const printItems = function(array) {
+//  for (let item of array) {
+//    console.log(item);
+//  }
+//}
+
+//  const array = ["😎", "💩", "🤗", "😼", "😂"];
+//  printItems(array);
+
+
+//Recursion code to print the array below same as an unested array 
+const printItems = function(array) {
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      // Print out all it's items individually
+      printItems(item);
+    } else {
+      console.log(item);
+    }
+  });
+}
+const array = ["😎", ["💩", "🤗"], "😼", "😂"];
 printItems(array);
+
+
+
+//logic:
+//  printItems(["😎", ["💩", "🤗"], "😼", "😂"]) {
+
+  // Item is not an array, so just log it
+//   console.log("😎")
+
+  // Item is an array so call the function again
+//    printItems(["💩", "🤗"]) {
+    // Item is not an array, so just log it
+ //    console.log("💩")
+    // Item is not an array, so just log it
+ //      console.log("🤗")
+ // }
+
+  // Item is not an array, so just log it
+  //   console.log("😼")
+
+  // Item is not an array, so just log it
+  //   console.log("😂")
+//}
---
+++ w1/d4-funcs/using_iteration_instead.js
@@ -0,0 +1,14 @@
+
+const printItems = function(array) {
+  while (array.length > 0) {
+    const element = array.shift();
+
+    if (Array.isArray(element)) {
+      element.reverse().forEach((newElement)=> {
+        array.unshift(newElement);
+      });
+    } else {
+      console.log(element);
+    }
+  }
+}
